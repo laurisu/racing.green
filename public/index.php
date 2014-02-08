b52f474bdf65f48deaa948af14dd161f15d8fe73
Lauris <Lauris@Lauris-PC>
2014-02-08 14:20:32 +0200

    ADD: twig

--- public/index.php
+++ public/index.php
@@ -1,10 +1,27 @@
 <?php
 require '../vendor/autoload.php';
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'debug' => true,
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => '../templates',
+));
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 $app->get('/it', function () {
     echo "It works";
 });
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/', function () use ($app) {
+    $app->render('index.html.twig');
+})->name('home');
+
 $app->run();
