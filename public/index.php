e1abcc9648264bf9408a3e43a7c922c257e2cd74
Lauris <Lauris@Lauris-PC>
2014-02-08 13:24:07 +0200

    ADD: slim framework default config.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php';
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+$app->get('/it', function () {
+    echo "It works";
+});
+$app->run();
